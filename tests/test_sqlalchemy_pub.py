4c6c64d0993f8ab7407c9533f8e4ab74b1a1ac42
lxyu <i@lxyu.net>
2014-11-26 13:08:58 +0800

    rewrite sqlalchemy_pub test with pytest fixtures

--- tests/test_sqlalchemy_pub.py
+++ tests/test_sqlalchemy_pub.py
@@ -3,23 +3,30 @@
 from __future__ import absolute_import
 
 import logging
+import pytest
+
 logging.basicConfig(level=logging.DEBUG)
 
 from blinker import signal
 
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
 
-def test_sqlalchemy_pub(mysql_dsn):
-    (t_writes, t_updates, t_deletes,
-     s_events, s_commits, s_rollbacks) = ([] for _ in range(6))
+(t_writes, t_updates, t_deletes,
+ s_events, s_commits, s_rollbacks) = ([] for _ in range(6))
 
-    def _clear():
-        del t_writes[:]
-        del t_updates[:]
-        del t_deletes[:]
-        del s_events[:]
-        del s_commits[:]
-        del s_rollbacks[:]
 
+def _clear():
+    del t_writes[:]
+    del t_updates[:]
+    del t_deletes[:]
+    del s_events[:]
+    del s_commits[:]
+    del s_rollbacks[:]
+
+
+def setup_module(module):
     def test_sg(sg_list):
         return lambda pk: sg_list.append(pk)
 
@@ -35,109 +42,149 @@ def test_sqlalchemy_pub(mysql_dsn):
     signal("session_commit").connect(test_sg(s_commits), weak=False)
     signal("session_rollback").connect(test_sg(s_rollbacks), weak=False)
 
-    import sqlalchemy as sa
-    from sqlalchemy.orm import scoped_session, sessionmaker
-    from sqlalchemy.ext.automap import automap_base
 
+def teardown_module(module):
+    pass
+
+
+def setup_function(function):
+    _clear()
+
+
+def teardown_function(function):
+    pass
+
+
+@pytest.fixture(scope="module")
+def model_cls():
+    Base = declarative_base()
+
+    class model_cls(Base):
+        __tablename__ = "test"
+        id = sa.Column(sa.Integer, primary_key=True)
+        data = sa.Column(sa.String)
+    return model_cls
+
+
+@pytest.fixture(scope="module")
+def session(mysql_dsn):
     # sqlalchemy prepare
     engine = sa.create_engine(mysql_dsn)
-    base = automap_base()
-    base.prepare(engine=engine, reflect=True)
-    Session = scoped_session(sessionmaker(bind=engine, expire_on_commit=False))
-    Test = base.classes["test"]
+    session = scoped_session(sessionmaker(bind=engine, expire_on_commit=False))
 
     # install sqlalchemy_pub hook
     from meepo.pub import sqlalchemy_pub
-    sqlalchemy_pub(Session, strict_tables=["test"])
+    sqlalchemy_pub(session, strict_tables=["test"])
 
-    # test empty commit
-    _clear()
-    Session().commit()
+    return session
+
+
+def test_sa_empty_commit(session):
+    """Direct commit generates nothing
+    """
+    session.commit()
+
     assert [t_writes, t_updates, t_deletes,
             s_events, s_commits, s_rollbacks] == [[]] * 6
 
-    # test single write
-    _clear()
-    session = Session()
-    t_a = Test(data='a')
+
+def test_sa_single_write(session, model_cls):
+    """Write commit generate a write event with row pk.
+    """
+    t_a = model_cls(data='a')
     session.add(t_a)
     session.commit()
-    session.close()
 
     event, sid = s_events.pop(), s_commits.pop()
-    assert t_writes == [1]
-    assert event == {"sid": sid, "action": "write", "event": {"test": {1}}}
+    assert event == {
+        "sid": sid, "action": "write", "event": {"test": {t_a.id}}}
+
+    assert t_writes == [t_a.id]
     assert [t_updates, t_deletes, s_rollbacks] == [[]] * 3
 
-    # test single flush - write
-    _clear()
-    session = Session()
-    t_b = Test(data='b')
+
+def test_sa_single_flush_write(session, model_cls):
+    """Flush - Write is the same with write.
+    """
+    t_b = model_cls(data='b')
     session.add(t_b)
     session.flush()
     session.commit()
-    session.close()
 
     event, sid = s_events.pop(), s_commits.pop()
-    assert t_writes == [2]
-    assert event == {"sid": sid, "action": "write", "event": {"test": {2}}}
+    assert event == {
+        "sid": sid, "action": "write", "event": {"test": {t_b.id}}}
+
+    assert t_writes == [t_b.id]
     assert [t_updates, t_deletes, s_rollbacks] == [[]] * 3
 
+
+def test_sa_multi_writes(session, model_cls):
     # test multiple writes
-    _clear()
-    session = Session()
-    t_c = Test(data='c')
-    t_d = Test(data='d')
+    t_c = model_cls(data='c')
+    t_d = model_cls(data='d')
     session.add(t_c)
     session.add(t_d)
     session.commit()
-    session.close()
 
     event, sid = s_events.pop(), s_commits.pop()
-    assert set(t_writes) == {3, 4}
-    assert event == {"sid": sid, "action": "write", "event": {"test": {3, 4}}}
+    assert event == {"sid": sid,
+                     "action": "write",
+                     "event": {"test": {t_c.id, t_d.id}}}
+
+    assert set(t_writes) == {t_c.id, t_d.id}
     assert [t_updates, t_deletes, s_rollbacks] == [[]] * 3
 
+
+def test_sa_single_update(session, model_cls):
     # test single update
-    _clear()
-    session = Session()
-    t_a = session.query(Test).filter(Test.data == 'a').one()
+    t_a = session.query(model_cls).filter(model_cls.data == 'a').one()
     t_a.data = "aa"
     session.commit()
-    session.close()
 
     event, sid = s_events.pop(), s_commits.pop()
-    assert set(t_updates) == {1}
-    assert event == {"sid": sid, "action": "update", "event": {"test": {1}}}
+    assert event == {
+        "sid": sid, "action": "update", "event": {"test": {t_a.id}}}
+
+    assert set(t_updates) == {t_a.id}
     assert [t_writes, t_deletes, s_rollbacks] == [[]] * 3
 
+
+def test_sa_single_flush_update(session, model_cls):
     # test single flush - update
-    _clear()
-    session = Session()
-    t_a = session.query(Test).filter(Test.data == 'aa').one()
+    t_a = session.query(model_cls).filter(model_cls.data == 'aa').one()
     t_a.data = "a"
     session.flush()
     session.commit()
-    session.close()
 
     event, sid = s_events.pop(), s_commits.pop()
-    assert set(t_updates) == {1}
-    assert event == {"sid": sid, "action": "update", "event": {"test": {1}}}
+    assert event == {
+        "sid": sid, "action": "update", "event": {"test": {t_a.id}}}
+
+    assert set(t_updates) == {t_a.id}
     assert [t_writes, t_deletes, s_rollbacks] == [[]] * 3
 
-    # test mixed write, update, delete & multi flush
-    _clear()
-    session = Session()
-    t_b, t_c = session.query(Test).filter(Test.data.in_(('b', 'c'))).all()
-    t_e = Test(data='e')
+
+def test_sa_mixed_write_update_delete_and_multi_flushes(session, model_cls):
+    """The most compliated situation, the test goes through the following
+    process:
+    1. add one row, update one row
+    2. flush to database
+    3. delete one row
+    4. flush to database
+    5. commit
+    """
+    t_b, t_c = session.query(model_cls).\
+        filter(model_cls.data.in_(('b',  'c'))).all()
+    t_e = model_cls(data='e')
     session.add(t_e)
     t_b.data = "x"
     session.flush()
     session.delete(t_c)
+    session.flush()
     session.commit()
-    session.close()
 
-    # one success commit yields one commit sid
+    # one success commit generates one commit sid
     assert len(s_commits) == 1
 
     # since the commit include a flush in it, if a flush happened in the middle
@@ -148,41 +195,44 @@ def test_sqlalchemy_pub(mysql_dsn):
     # test session events
     sid = s_commits.pop()
     assert s_events[2:] == [
-        {"sid": sid, "action": "write", "event": {"test": {5}}},
-        {"sid": sid, "action": "update", "event": {"test": {2}}},
-        {"sid": sid, "action": "delete", "event": {"test": {3}}}
+        {"sid": sid, "action": "write", "event": {"test": {t_e.id}}},
+        {"sid": sid, "action": "update", "event": {"test": {t_b.id}}},
+        {"sid": sid, "action": "delete", "event": {"test": {t_c.id}}}
     ]
-    assert (t_writes, t_updates, t_deletes) == ([5], [2], [3])
+    assert (t_writes, t_updates, t_deletes) == ([t_e.id], [t_b.id], [t_c.id])
 
-    # test empty rollback
-    _clear()
-    Session().rollback()
+
+def test_sa_empty_rollback(session):
+    """Direct rollback generates nothing
+    """
+    session.rollback()
+
     assert [t_writes, t_updates, t_deletes,
             s_events, s_commits, s_rollbacks] == [[]] * 6
 
-    # test rollback before flush
-    _clear()
-    session = Session()
-    t_e = Test(data='e')
+
+def test_sa_early_rollback(session, model_cls):
+    """Rollback happened before flush, nothing recorded.
+    """
+    t_e = model_cls(data='e')
     session.add(t_e)
     session.rollback()
-    session.close()
 
-    # rollback happened before flush, nothing recorded.
     assert [t_writes, t_updates, t_deletes,
             s_events, s_commits, s_rollbacks] == [[]] * 6
 
-    # test flush & rollback
-    _clear()
-    session = Session()
-    t_e = Test(data='e')
+
+def test_sa_flush_rollback(session, model_cls):
+    """Rollback happened after flush, event recorded.
+    Since rollback happened after flush, the write have a pk value.
+    """
+    t_e = model_cls(data='e')
     session.add(t_e)
     session.flush()
     session.rollback()
-    session.close()
 
-    # test session events, since rollback happened after flush, the write
-    # have a pk value.
     event, sid = s_events.pop(), s_rollbacks.pop()
-    assert event == {"sid": sid, "action": "write", "event": {"test": {6}}}
+    assert event == {
+        "sid": sid, "action": "write", "event": {"test": {t_e.id}}}
+
     assert [t_writes, t_updates, t_deletes, s_commits] == [[]] * 4
