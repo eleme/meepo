58f701d302c882f00d3ca9691e7b69b160f46f04
lxyu <i@lxyu.net>
2014-11-26 13:23:14 +0800

    rewrite mysql_pub test with pytest fixture.

--- tests/test_mysql_pub.py
+++ tests/test_mysql_pub.py
@@ -6,33 +6,37 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 import pymysql
+import pytest
+
 from blinker import signal
 
 from meepo._compat import urlparse
+from meepo.pub import mysql_pub
+
+t_writes, t_updates, t_deletes, t_binlogs = [], [], [], []
+t_raw_writes, t_raw_updates, t_raw_deletes = [], [], []
 
 
-def test_mysql_pub(mysql_dsn):
-    logger = logging.getLogger("test_mysql_pub")
-
+def setup_module(module):
     def test_sg(sg_list):
         return lambda pk: sg_list.append(pk)
 
     # connect table action signal
-    test_write_l, test_update_l, test_delete_l = [], [], []
-    signal("test_write").connect(test_sg(test_write_l), weak=False)
-    signal("test_update").connect(test_sg(test_update_l), weak=False)
-    signal("test_delete").connect(test_sg(test_delete_l), weak=False)
+    signal("test_write").connect(test_sg(t_writes), weak=False)
+    signal("test_update").connect(test_sg(t_updates), weak=False)
+    signal("test_delete").connect(test_sg(t_deletes), weak=False)
 
     # connect raw table action signal
-    test_write_raw_l, test_update_raw_l, test_delete_raw_l = [], [], []
-    signal("test_write_raw").connect(test_sg(test_write_raw_l), weak=False)
-    signal("test_update_raw").connect(test_sg(test_update_raw_l), weak=False)
-    signal("test_delete_raw").connect(test_sg(test_delete_raw_l), weak=False)
+    signal("test_write_raw").connect(test_sg(t_raw_writes), weak=False)
+    signal("test_update_raw").connect(test_sg(t_raw_updates), weak=False)
+    signal("test_delete_raw").connect(test_sg(t_raw_deletes), weak=False)
 
     # connect mysql binlog pos signal
-    test_binlog_l = []
-    signal("mysql_binlog_pos").connect(test_sg(test_binlog_l),  weak=False)
+    signal("mysql_binlog_pos").connect(test_sg(t_binlogs),  weak=False)
 
+
+@pytest.fixture(scope="module")
+def binlog(mysql_dsn):
     # init mysql connection
     parsed = urlparse(mysql_dsn)
     db_settings = {
@@ -47,6 +51,8 @@ def test_mysql_pub(mysql_dsn):
 
     # test sqls
     sql = """
+    TRUNCATE TABLE meepo_test.test;
+    RESET MASTER;
     INSERT INTO test (data) VALUES ('a');
     INSERT INTO test (data) VALUES ('b'), ('c'), ('d');
     UPDATE test SET data = 'aa' WHERE id = 1;
@@ -59,28 +65,33 @@ def test_mysql_pub(mysql_dsn):
     cursor.close()
     conn.commit()
     conn.close()
-    logger.debug("mysql binlog generated")
 
-    from meepo.pub import mysql_pub
+    # generates signals
     mysql_pub(mysql_dsn, tables=["test"])
 
-    # test pk signal
-    assert test_write_l == [1, 2, 3, 4]
-    assert test_update_l == [1, 2, 2, 3, 4]
-    assert test_delete_l == [2, 3, 4, 1]
-    assert test_binlog_l == ['mysql-bin.000001:270', 'mysql-bin.000001:375',
-                             'mysql-bin.000001:474', 'mysql-bin.000001:573',
-                             'mysql-bin.000001:707', 'mysql-bin.000001:815',
-                             'mysql-bin.000001:905']
 
-    # test raw data signal
-    assert test_write_raw_l == [
+def test_mysql_table_event(binlog):
+    assert t_writes == [1, 2, 3, 4]
+    assert t_updates == [1, 2, 2, 3, 4]
+    assert t_deletes == [2, 3, 4, 1]
+
+
+def test_mysql_binlog_pos_event(binlog):
+    assert t_binlogs == [
+        'mysql-bin.000001:270', 'mysql-bin.000001:375',
+        'mysql-bin.000001:474', 'mysql-bin.000001:573',
+        'mysql-bin.000001:707', 'mysql-bin.000001:815',
+        'mysql-bin.000001:905']
+
+
+def test_mysql_raw_table_event(binlog):
+    assert t_raw_writes == [
         {'values': {'data': 'a', 'id': 1}},
         {'values': {'data': 'b', 'id': 2}},
         {'values': {'data': 'c', 'id': 3}},
         {'values': {'data': 'd', 'id': 4}},
     ]
-    assert test_update_raw_l == [
+    assert t_raw_updates == [
         {'before_values': {'data': 'a', 'id': 1},
          'after_values': {'data': 'aa', 'id': 1}},
         {'before_values': {'data': 'b', 'id': 2},
@@ -92,7 +103,7 @@ def test_mysql_pub(mysql_dsn):
         {'before_values': {'data': 'd', 'id': 4},
          'after_values': {'data': 'cc', 'id': 4}},
     ]
-    assert test_delete_raw_l == [
+    assert t_raw_deletes == [
         {'values': {'data': 'cc', 'id': 2}},
         {'values': {'data': 'cc', 'id': 3}},
         {'values': {'data': 'cc', 'id': 4}},
