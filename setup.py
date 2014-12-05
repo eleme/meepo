05b9769907e8930868fb407752a94161bfbc7115
lxyu <i@lxyu.net>
2014-12-05 17:11:31 +0800

    simplified setup.py requirements

--- setup.py
+++ setup.py
@@ -3,42 +3,23 @@
 
 from setuptools import setup, find_packages
 
-# console_scripts
-cmds = [
-    "meventsourcing = meepo.apps.meventsourcing:main",
-    "mnano = meepo.apps.mnano:main",
-    "mprint = meepo.apps.mprint:main",
-    "mreplicate = meepo.apps.mreplicate:main",
-    "mzdevice = meepo.apps.mzdevice:main",
-    "mzmq = meepo.apps.mzmq:main",
-]
-
-
 # requirements
 install_requires = [
     "SQLAlchemy>=0.9.0,<1.0.0",
     "blinker>=1.3,<2.0",
-    "click>=3.3,<4.0",
+    "mysql-replication>=0.5,<0.6.0",
+    "pyketama>=0.2.0",
     "pyzmq>=14.4.1,<15.0.0",
     "redis>=2.10.3,<2.11.0",
-    "pyketama>=0.2.0",
-]
-
-mysqlbinlog_requires = [
-    "mysql-replication>=0.5,<0.6.0",
-]
-
-# nanomsg is still in beta
-nanomsg_requires = [
-    "nanomsg",
 ]
 
 dev_requires = [
-    "flake8>=2.2.1",
-    "pytest>=2.6.1",
+    "flake8>=2.2",
+    "pytest>=2.6",
     "sphinx-rtd-theme>=0.1.6",
-    "sphinx>=1.2.2",
-] + mysqlbinlog_requires
+    "sphinx>=1.2",
+    "tox>=1.8",
+] + install_requires
 
 
 setup(name="meepo",
@@ -48,7 +29,6 @@ setup(name="meepo",
       author="Lx Yu",
       author_email="i@lxyu.net",
       packages=find_packages(exclude=['docs', 'tests']),
-      entry_points={"console_scripts": cmds},
       url="https://github.com/eleme/meepo",
       license="MIT",
       zip_safe=False,
@@ -56,8 +36,6 @@ setup(name="meepo",
       install_requires=install_requires,
       extras_require={
           "dev": dev_requires,
-          "mysqlbinlog": mysqlbinlog_requires,
-          "nano": nanomsg_requires,
       },
       classifiers=[
           "Topic :: Software Development",
