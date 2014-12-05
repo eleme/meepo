14650a0567b20e43b2101de66404fb736272b0fc
lxyu <i@lxyu.net>
2014-12-05 17:11:55 +0800

    support green zmq (for gevent)

--- meepo/sub/zmq.py
+++ meepo/sub/zmq.py
@@ -3,10 +3,9 @@
 import logging
 
 from blinker import signal
-import zmq
 
 
-def zmq_sub(bind, tables, forwarder=False):
+def zmq_sub(bind, tables, forwarder=False, green=False):
     """0mq fanout sub.
 
     This sub will use zeromq to fanout the events.
@@ -14,22 +13,28 @@ def zmq_sub(bind, tables, forwarder=False):
     :param bind: the zmq pub socket or zmq device socket.
     :param tables: the events of tables to follow.
     :param forwarder: set to True if zmq pub to a forwarder device.
+    :param green: weather to use a greenlet compat zmq
     """
     logger = logging.getLogger("meepo.sub.nano_sub")
 
+    if not green:
+        import zmq
+    else:
+        from zmq.green import zmq
+
     ctx = zmq.Context()
-    pub_socket = ctx.socket(zmq.PUB)
+    socket = ctx.socket(zmq.PUB)
 
     if forwarder:
-        pub_socket.connect(bind)
+        socket.connect(bind)
     else:
-        pub_socket.bind(bind)
+        socket.bind(bind)
 
     def _sub(table):
         for action in ("write", "update", "delete"):
             def _sub(pk, action=action):
                 msg = "%s_%s %s" % (table, action, pk)
-                pub_socket.send_string(msg)
+                socket.send_string(msg)
                 logger.debug("pub msg: %s" % msg)
             signal("%s_%s" % (table, action)).connect(_sub, weak=False)
 
