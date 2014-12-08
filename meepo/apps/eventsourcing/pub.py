305196e121bc19bf4bcf68f2fb85e98db2605c6f
lxyu <i@lxyu.net>
2014-12-08 16:12:52 +0800

    rewrite sqlalchemy es_pub with cls and inheritance

--- meepo/apps/eventsourcing/pub.py
+++ meepo/apps/eventsourcing/pub.py
@@ -9,17 +9,17 @@ Pubs for meepo eventsourcing app.
 
 from __future__ import absolute_import
 
-import collections
 import logging
+
+import collections
 
 from blinker import signal
 from sqlalchemy import event
 
-from ...pub.sqlalchemy import (
-    session_init, session_update, session_del, session_pub, sa_pk)
+from ...pub.sqlalchemy import MSQLAlchemyPub
 
 
-def sqlalchemy_es_pub(session, tables=None):
+class MSQLAlchemyEventSourcingPub(MSQLAlchemyPub):
     """SQLAlchemy EventSourcing Pub.
 
     Add eventsourcing to sqlalchemy_pub, three more signals added for tables:
@@ -29,62 +29,88 @@ def sqlalchemy_es_pub(session, tables=None):
 
     The hook will use prepare-commit pattern to ensure 100% reliability on
     event sourcing.
-
-    :param session: sqlalchemy session.
-    :param tables: which tables to enable sqlalchemy_es_pub.
     """
-    logger = logging.getLogger("meepo.apps.es.pub.sqlalchemy_pub")
+    logger = logging.getLogger("meepo.apps.eventsourcing.sqlalchemy_pub")
 
-    # enable session_update hook
-    event.listen(session, "before_flush", session_update)
-
-    # enable es session_prepare hook
-    def _session_prepare(session, flush_ctx):
+    @classmethod
+    def session_prepare(cls, session, _):
         """Record session prepare state in before_commit
         """
         if not hasattr(session, 'meepo_unique_id'):
-            session_init(session)
+            cls.session_init(session)
 
-        logger.debug("%s - after_flush" % session.meepo_unique_id)
-
+        tables = getattr(session, "_meepo_sqlalchemy_es_pub_tables", [])
         evt = collections.defaultdict(set)
         for action in ("write", "update", "delete"):
-            for obj in [o for o in getattr(session, "pending_%s" % action)
-                        if o.__table__.fullname in tables]:
+            objs = getattr(session, "pending_%s" % action)
+            # filter tables if possible
+            if tables:
+                objs = [o for o in objs if o.__table__.fullname in tables]
+            for obj in objs:
                 evt_name = "%s_%s" % (obj.__table__.fullname, action)
-                evt[evt_name].add(sa_pk(obj))
-                logger.debug("%s - session_prepare %s -> %s".format(
+                evt[evt_name].add(cls._pk(obj))
+                cls.logger.debug("%s - session_prepare: %s -> %s".format(
                     session.meepo_unique_id, evt_name, evt))
         signal("session_prepare").send(session, event=evt)
-    event.listen(session, "after_flush", _session_prepare)
 
-    # enable es session_commit hook
-    def _session_commit(session):
+    @classmethod
+    def session_commit(cls, session):
         """Commit session in after_commit
         """
         # this may happen when there's nothing to commit
         if not hasattr(session, 'meepo_unique_id'):
-            logger.debug("skipped - after_commit")
+            cls.logger.debug("skipped - session_commit")
             return
 
         # normal session pub
-        logger.debug("%s - after_commit" % session.meepo_unique_id)
-        session_pub(session)
+        cls.logger.debug("%s - session_commit" % session.meepo_unique_id)
+        cls.session_pub(session)
         signal("session_commit").send(session)
-        session_del(session)
-    event.listen(session, "after_commit", _session_commit)
+        cls.session_del(session)
 
-    # enable es session_rollback hook
-    def _session_rollback(session):
+    @classmethod
+    def session_rollback(cls, session):
         """Clean session in after_rollback.
         """
         # this may happen when there's nothing to rollback
         if not hasattr(session, 'meepo_unique_id'):
-            logger.debug("skipped - after_rollback")
+            cls.logger.debug("skipped - session_rollback")
             return
 
         # del session meepo id after rollback
-        logger.debug("%s - after_rollback" % session.meepo_unique_id)
+        cls.logger.debug("%s - after_rollback" % session.meepo_unique_id)
         signal("session_rollback").send(session)
-        session_del(session)
-    event.listen(session, "after_rollback", _session_rollback)
+        cls.session_del(session)
+
+    @classmethod
+    def install(cls, session, tables=None):
+        """Install hook
+
+        :param session: sqlalchemy session to install the hook
+        :param tables: tables to install the hook, leave None to pub all.
+        """
+        cls.logger.debug("session_install - eventsourcing")
+
+        if not hasattr(session, "_meepo_sqlalchemy_es_pub_tables"):
+            # init es listen tables
+            session._meepo_sqlalchemy_es_pub_tables = set()
+
+            # enable session_update hook
+            event.listen(session, "before_flush", cls.session_update)
+
+            # enable session prepare-commit hook
+            event.listen(session, "after_flush", cls.session_prepare)
+            event.listen(session, "after_commit", cls.session_commit)
+            event.listen(session, "after_rollback", cls.session_rollback)
+
+        if tables:
+            session._meepo_sqlalchemy_es_pub_tables |= set(tables)
+
+
+def sqlalchemy_es_pub(session, tables=None):
+    """Install sqlalchemy eventsourcing hook by MSQLAlchemyEventSourcingPub.
+
+    :param session: sqlalchemy session.
+    :param tables: which tables to enable sqlalchemy_es_pub.
+    """
+    MSQLAlchemyEventSourcingPub().install(session, tables)
--- meepo/pub/sqlalchemy.py
+++ meepo/pub/sqlalchemy.py
@@ -3,7 +3,6 @@
 from __future__ import absolute_import
 
 import logging
-logger = logging.getLogger("meepo.pub.sqlalchemy_pub")
 
 import uuid
 
@@ -11,104 +10,135 @@ from blinker import signal
 from sqlalchemy import event
 
 
-def sa_pk(obj):
-    """Get pk values from object
+class MSQLAlchemyPub(object):
+    """SQLAlchemy Pub.
 
-    :param obj: sqlalchemy object
+    This publisher don't publish events itself, it will hook on SQLAlchemy's
+    event system, and publish the changes automatically.
+
+    You may customize the sqlalchemy pub behavior, refer to
+    MSQLAlchemyEventSourcingPub for example.
     """
-    pk_values = tuple(getattr(obj, c.name)
-                      for c in obj.__mapper__.primary_key)
-    if len(pk_values) == 1:
-        return pk_values[0]
-    return pk_values
 
+    logger = logging.getLogger("meepo.pub.sqlalchemy_pub")
 
-def session_init(session, *args, **kwargs):
-    if hasattr(session, "meepo_unique_id"):
-        logger.debug("skipped - session_init")
-        return
-
-    for action in ("write", "update", "delete"):
-        attr = "pending_{}".format(action)
-        if not hasattr(session, attr):
-            setattr(session, attr, set())
-    session.meepo_unique_id = uuid.uuid4().hex
-    logger.debug("%s - session_init" % session.meepo_unique_id)
-
-
-def session_del(session):
-    del session.meepo_unique_id
-    del session.pending_write
-    del session.pending_update
-    del session.pending_delete
-
-
-def session_update(session, flush_ctx, nonsense):
-    """Record session changes on flush
-    """
-    session_init(session)
-    logger.debug("%s - before_flush" % session.meepo_unique_id)
-    session.pending_write |= set(session.new)
-    session.pending_update |= set(session.dirty)
-    session.pending_delete |= set(session.deleted)
-
-
-def session_pub(session):
-    def _pub(obj, action, tables=None):
-        """Publish object pk values with action.
-
-        The _pub will trigger 2 signals:
-        * normal ``table_action`` signal, sends primary key
-        * raw ``table_action_raw`` signal, sends sqlalchemy object
+    @classmethod
+    def _pk(cls, obj):
+        """Get pk values from object
 
         :param obj: sqlalchemy object
-        :param action: action on object
         """
-        if tables and obj.__tablename__ not in tables:
+        pk_values = tuple(getattr(obj, c.name)
+                          for c in obj.__mapper__.primary_key)
+        if len(pk_values) == 1:
+            return pk_values[0]
+        return pk_values
+
+    @classmethod
+    def session_init(cls, session):
+        if hasattr(session, "meepo_unique_id"):
+            cls.logger.debug("skipped - session_init")
             return
 
-        sg_name = "{}_{}".format(obj.__table__, action)
-        sg = signal(sg_name)
-        sg_raw = signal("{}_raw".format(sg_name))
+        for action in ("write", "update", "delete"):
+            attr = "pending_{}".format(action)
+            if not hasattr(session, attr):
+                setattr(session, attr, set())
+        session.meepo_unique_id = uuid.uuid4().hex
+        cls.logger.debug("%s - session_init" % session.meepo_unique_id)
 
-        pk = sa_pk(obj)
-        if pk:
-            sg.send(pk)
-            sg_raw.send(obj)
-            logger.debug("{} -> {}".format(sg_name, pk))
+    @classmethod
+    def session_del(cls, session):
+        cls.logger.debug("%s - session_del" % session.meepo_unique_id)
+        del session.meepo_unique_id
+        del session.pending_write
+        del session.pending_update
+        del session.pending_delete
 
-    logger.debug("pub_session")
-    for obj in session.pending_write:
-        _pub(obj, action="write")
-    for obj in session.pending_update:
-        _pub(obj, action="update")
-    for obj in session.pending_delete:
-        _pub(obj, action="delete")
+    @classmethod
+    def session_update(cls, session, *_):
+        """Record session changes on flush
+        """
+        cls.session_init(session)
+        session.pending_write |= set(session.new)
+        session.pending_update |= set(session.dirty)
+        session.pending_delete |= set(session.deleted)
+        cls.logger.debug("%s - session_update" % session.meepo_unique_id)
 
-    session.pending_write.clear()
-    session.pending_update.clear()
-    session.pending_delete.clear()
+    @classmethod
+    def session_pub(cls, session):
+        def _pub(obj, action):
+            """Publish object pk values with action.
 
+            The _pub will trigger 2 signals:
+            * normal ``table_action`` signal, sends primary key
+            * raw ``table_action_raw`` signal, sends sqlalchemy object
 
-def session_commit(session):
-    # this may happen when there's nothing to commit
-    if not hasattr(session, 'meepo_unique_id'):
-        logger.debug("skipped - after_commit")
-        return
+            :param obj: sqlalchemy object
+            :param action: action on object
+            """
+            tables = getattr(session, "_meepo_sqlalchemy_pub_tables", [])
+            if tables and obj.__table__.fullname not in tables:
+                return
 
-    session_pub(session)
-    session_del(session)
+            sg_name = "{}_{}".format(obj.__table__, action)
+            sg = signal(sg_name)
+            sg_raw = signal("{}_raw".format(sg_name))
+
+            pk = cls._pk(obj)
+            if pk:
+                sg.send(pk)
+                sg_raw.send(obj)
+                cls.logger.debug("%s - session_pub: %s -> %s" % (
+                    session.meepo_unique_id, sg_name, pk))
+
+        for obj in session.pending_write:
+            _pub(obj, action="write")
+        for obj in session.pending_update:
+            _pub(obj, action="update")
+        for obj in session.pending_delete:
+            _pub(obj, action="delete")
+
+        session.pending_write.clear()
+        session.pending_update.clear()
+        session.pending_delete.clear()
+
+    @classmethod
+    def session_commit(cls, session):
+        # this may happen when there's nothing to commit
+        if not hasattr(session, 'meepo_unique_id'):
+            cls.logger.debug("skipped - session_commit")
+            return
+
+        cls.session_pub(session)
+        cls.session_del(session)
+
+    @classmethod
+    def install(cls, session, tables=None):
+        """Install hook
+
+        :param session: sqlalchemy session to install the hook
+        :param tables: tables to install the hook, leave None to pub all.
+        """
+        cls.logger.debug("session_install")
+
+        if not hasattr(session, "_meepo_sqlalchemy_pub_tables"):
+            # init listen tables
+            session._meepo_sqlalchemy_pub_tables = set()
+
+            # enable session_update & session_commit hook
+            event.listen(session, "before_flush", cls.session_update)
+            event.listen(session, "after_commit", cls.session_commit)
+
+        if tables:
+            session._meepo_sqlalchemy_pub_tables |= set(tables)
 
 
 def sqlalchemy_pub(session, tables=None):
-    """SQLAlchemy Pub.
-
-    This publisher don't publish events itself, it will hook on sqlalchemy's
-    event system, and publish the changes automatically.
+    """Install sqlalchemy eventsourcing hook by MSQLAlchemyPub.
 
     :param session: sqlalchemy db session.
     :param tables: which tables to enable sqlalchemy_pub.
     """
-    # enable session_update & session_commit hook
-    event.listen(session, "before_flush", session_update)
-    event.listen(session, "after_commit", session_commit)
+    # install sqlalchemy_pub hook
+    MSQLAlchemyPub().install(session, tables)
